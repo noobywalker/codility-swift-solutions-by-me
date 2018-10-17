3a98d18e195119a336b82316de9d31850a26479c
Waratnan Suriyasorn <s.waratnan@gmail.com>
2018-10-18 01:07:16 +0700

    PermMissingElem solution

---
+++ codility-solutions.playground/Pages/PermMissingElem.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+public func solution(_ A : inout [Int]) -> Int {
+        var numbers = [Int?](count: A.count + 1, repeatingValue: nil)
+        for (_, number) in A.enumerated() {
+            numbers[number - 1] = number
+        }
+
+        for (idx, number) in numbers.enumerated() {
+            if number == nil {
+                return idx + 1
+            }
+        }
+        return 0
+    }
+}
